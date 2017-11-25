d3c16a102472f7e8c53be50e1294dd554e3a4a58
Darival <darivalrcv@gmail.com>
2017-11-24 18:14:45 -0600

    add flow y asi

--- OpenWorkForce.java
+++ OpenWorkForce.java
@@ -4,34 +4,54 @@ import java.util.Scanner;
 
 class OpenWorkForce {
     public static void main(String[] args){
-        bienvenida();
+        Usuario user = bienvenida();
+    }
+
+    private static Usuario bienvenida(){
         Usuario user = null;
-        if (user == null) {
-            
-        }
-    }
-
-    private static void bienvenida(){
-        Scanner sc = new Scanner(System.in);
         System.out.println("Bienvenido a OpenWorkforce");
-        System.out.println("Login[L] \t Register[R]");
-        String option = sc.next();
-        System.out.println(option);
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Login[L] \t Register[R]");
+            String option = sc.next().toUpperCase();
+            sc.close();
+            /* if(option == "L"){
+                user = login();
+            } */
+            if(option == "R"){
+                user = registroDeUsuario();
+            }
+        } while (user == null);
+        return user;
     }
 
-    private static void iniciarSession(String email, String password){
+    private static Usuario authUser(String email, String password){
+        return new Cliente("test","test", "test");
+    }
 
+    private static Usuario registroDeUsuario(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nombre:");
+        String name = sc.next();
+        System.out.println("Correo:");
+        String email = sc.next().toLowerCase();
+        System.out.println("Contraseña:");
+        String password = sc.next();
+        sc.close();
+        return new Cliente(name, email, password);
     }
 
     private static void cerrarSession(){
 
     }
 
-    private static void registroDeUsuario(){
-
-    }
-
-    private static void inicio(){
-
-    }
+    /* private static void login(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Correo:");
+        String email = sc.next().toLowerCase();
+        System.out.println("Contraseña:");
+        String password = sc.next();
+        sc = close();
+        Usuario user = authUser(email, password);
+    } */
 }
