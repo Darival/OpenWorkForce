b2b7b9ee5329e1d9e0608c16fc690cb0406892dc
Darival <darivalrcv@gmail.com>
2017-11-30 11:29:37 -0600

    define la clase recibo

--- Entidades/Recibo.java
+++ Entidades/Recibo.java
@@ -1,5 +1,22 @@
 package Entidades;
 
 class Recibo {
+    private float total;
+    private boolean pagado = false;
 
+    public void setTotal(float total){
+        this.total = total;
+    }
+
+    public float getTotal(){
+        return total;
+    }
+
+    public boolean isPaid(){
+        return pagado;
+    }
+    
+    public pagar(){
+        pagado = true;
+    }
 }
