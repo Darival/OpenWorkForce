e2145f36f923458bacb95d6bed8188812f44f042
Darival <darivalrcv@gmail.com>
2017-11-30 12:05:58 -0600

    define la clase empleado

--- Entidades/Empleado.java
+++ Entidades/Empleado.java
@@ -1,5 +1,14 @@
 package Entidades;
 
 public class Empleado extends Usuario{
-    
+    private String servicio;
+    private float ingresos;
+
+    public void cobrar(float ingresos){
+        this.ingresos += ingresos;
+    }
+
+    public float ingresoAcumuladoMensual(){
+        return ingresos;
+    }
 }
